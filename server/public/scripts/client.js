efce4212c1e05771f255120b04e62c1018f5ae66
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-30 17:31:47 -0500

    made changes to the GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,20 @@ $(document).ready(onReady);
 function onReady() {
    console.log('inside the onReady function');
    $('#submit-btn').on('click', sumbit);
+   $('.isComplete').on('click', '.complete-btn', isComplete);
 
+}
+function isComplete(){
+   const isCompleteId = $(this).parent().data('id');
+   console.log('update the  complete status with id:', isCompleteId );
+   $.ajax({
+      method:'PUT',
+      url:`todo/${isCompleteId}`
+   }).then((response) => {
+      getTask();
+   }).catch((error) => {
+      console.log(error)
+   })
 }
 
 function sumbit(){
@@ -25,18 +38,23 @@ function sumbit(){
 }
 
 function getTask(){
-   $('#taskList').empty();
+   $('#taskListBody').empty();
+   // console.log('inside getTask');
    $.ajax({
       type: 'GET',
       url: '/todo'
    }).then((response) =>  {
-      console.log('inside GET', response);
+      // console.log('inside GET', response);
       // append data to the DOM
       for (let i = 0; i < response.length; i++) {
+         // let results
          let newRow = $(`
              <tr>
                  <td>${response[i].task}</td>
-                 <td>${response[i].isComplete}</td>
+                 <td>${response[i].isComplete} 
+                  </td>
+                  <td class = "isComplete">
+                  </td>
                  <td>
                      <button class="delete-btn">
                          Delete
@@ -48,11 +66,22 @@ function getTask(){
          // setter
          newRow.data('id',response[i].id)
          $('#taskList').append(newRow);
+         if(JSON.stringify(response[i].isComplete) == 'false'){
+            $('.isComplete').empty();
+            $('.isComplete').append(`
+               <button class ="complete-btn">
+                  complete
+               </button>
+            `)
+         };
      }
+    
    }).catch((error) => {
       console.log('Error in the GET', error);
    })
 }
+
+function 
 
 
 
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,7 @@ const express = require("express");
 // Check the Name 🔽
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/todo', (req, res) => {
     // Do a query to select all task from todo table
     console.log('insde the GET');
     let queryText = `SELECT * FROM "TODO"; `;
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     // Use the pool to connect with DB
     pool.query(queryText)
         .then((result) => {
-            console.log(result)
+            // console.log(result)
             res.send(result.rows)
         })
         .catch((error) => {
@@ -19,5 +19,35 @@ router.get('/', (req, res) => {
             res.sendStatus(500)
         })
 });
+
+router.post('/todo', (req,res) => {
+    console.log('req.body:', req.body)
+
+    const task = req.body.task
+    const isComplete = req.body.isComplete
+
+    const queryParams = [task,isComplete]
+    const queryText = `INSERT INTO "TODO" 
+                ("task", "isComplete") 
+                VALUES ($1, $2);`;
+    pool.query(queryText, queryParams)
+        .then((result)  => {
+            console.log('Pool query is working');
+            res.sendStatus(201)
+        }).catch((error) => {
+            console.log("Error on POST", error);
+            res.sendStatus(500)
+        })
+})
+
+router.put('/todo/:id', (req,res) =>{
+    console.log("req.parms:", req.params)
+
+    let isCompleteId = req.params.id;
+    let queryParams = []
+})
+
+
+
 
 module.exports = router;
