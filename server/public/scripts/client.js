ceec95f59581db4f0a4788c2097a3dea6f1f64cd
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-31 21:33:13 -0500

    made changes to the ajaxGET,POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,50 +1,43 @@
-const { response } = require("express");
-
 $(document).ready(onReady);
 
 function onReady() {
    console.log('inside the onReady function');
-   $('#submit-btn').on('click', sumbit);
+   $('#submit-btn').on('click', handleSumbit);
    $('.isComplete').on('click', '.complete-btn', isComplete);
 
 }
-function isComplete(){
-   const isCompleteId = $(this).parent().data('id');
-   console.log('update the  complete status with id:', isCompleteId );
-   $.ajax({
-      method:'PUT',
-      url:`todo/${isCompleteId}`
-   }).then((response) => {
-      getTask();
-   }).catch((error) => {
-      console.log(error)
-   })
-}
 
-function sumbit(){
-   let taskListObject = {
-      task : $('#task').val(),
-      isComplete : $('#isItComplete').val()
+function handleSumbit(){
+   //get the value from the input field
+   let task = { 
+      task: $('#task').val(),
+      isComplete: $('#isItComplete').val()
+
    }
-   $.ajax({
-      type:'POST',
-      url: '/todo',
-      data: taskListObject
-   }).then((response) => {
-      getTask();
-   }).catch((error) => {
-      console.log('Error on the POST', error);
-   })
+   console.log('the  inputs are:', task.task,task.isComplete)
+
+   addTask(task);
+
+   //empty the  input field
+   $('#task').val('');
+   $('#isItComplete').val('');
 }
 
 function getTask(){
    $('#taskListBody').empty();
-   // console.log('inside getTask');
+
    $.ajax({
-      type: 'GET',
+      method: 'GET',
       url: '/todo'
-   }).then((response) =>  {
-      // console.log('inside GET', response);
+   }).then((response)  => {
+      render(response)
+   }).catch((error) => {
+      console.log('Error with the GET', error)
+   })
+}
+
+function render(response){
+    console.log('inside GET', response);
       // append data to the DOM
       for (let i = 0; i < response.length; i++) {
          // let results
@@ -66,17 +59,94 @@ function getTask(){
          // setter
          newRow.data('id',response[i].id)
          $('#taskList').append(newRow);
-         if(JSON.stringify(response[i].isComplete) == 'false'){
-            $('.isComplete').empty();
-            $('.isComplete').append(`
-               <button class ="complete-btn">
-                  complete
-               </button>
-            `)
-         };
-     }
-    
+      }
+
+}
+
+function addTask(task){
+   $.ajax({
+      type:'POST',
+      url: '/todo',
+      data: task
+   }).then((response) => {
+      getTask();
    }).catch((error) => {
-      console.log('Error in the GET', error);
+      console.log('Error on the POST', error);
    })
 }
+
+
+
+
+function isComplete(){
+   const isCompleteId = $(this).parent().data('id');
+   console.log('update the  complete status with id:', isCompleteId );
+   $.ajax({
+      method:'PUT',
+      url:`todo/${isCompleteId}`
+   }).then((response) => {
+      getTask();
+   }).catch((error) => {
+      console.log(error)
+   })
+}
+
+// function sumbit(){
+//    let taskListObject = {
+//       task : $('#task').val(),
+//       isComplete : $('#isItComplete').val()
+//    }
+//    $.ajax({
+//       type:'POST',
+//       url: '/todo',
+//       data: taskListObject
+//    }).then((response) => {
+//       getTask();
+//    }).catch((error) => {
+//       console.log('Error on the POST', error);
+//    })
+// }
+
+// function getTask(){
+//    $('#taskListBody').empty();
+//    // console.log('inside getTask');
+//    $.ajax({
+//       type: 'GET',
+//       url: '/todo'
+//    }).then((response) =>  {
+//       // console.log('inside GET', response);
+//       // append data to the DOM
+//       for (let i = 0; i < response.length; i++) {
+//          // let results
+//          let newRow = $(`
+//              <tr>
+//                  <td>${response[i].task}</td>
+//                  <td>${response[i].isComplete} 
+//                   </td>
+//                   <td class = "isComplete">
+//                   </td>
+//                  <td>
+//                      <button class="delete-btn">
+//                          Delete
+//                      </button>
+//                  </td>
+//              </tr>
+//          `)
+
+//          // setter
+//          newRow.data('id',response[i].id)
+//          $('#taskList').append(newRow);
+//          if(JSON.stringify(response[i].isComplete) == 'false'){
+//             $('.isComplete').empty();
+//             $('.isComplete').append(`
+//                <button class ="complete-btn">
+//                   complete
+//                </button>
+//             `)
+//          };
+//      }
+    
+//    }).catch((error) => {
+//       console.log('Error in the GET', error);
+//    })
+// }
